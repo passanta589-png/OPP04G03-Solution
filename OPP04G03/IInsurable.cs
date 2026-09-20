20a3775f3928f56a4b5c78a62c1f0aacaa65dae7
passant ahmed <passantahmed992006@outlook.com>
2026-09-21 00:39:46 +0300

    Q05

---
+++ OPP04G03/IInsurable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OPP04G03
+{
+    internal interface IInsurable
+    {
+        decimal CalculateInsurance();
+    }
+}
