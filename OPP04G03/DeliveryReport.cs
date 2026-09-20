99617cd6482cf75e4cd9916cc6e6d643ee0c271d
passant ahmed <passantahmed992006@outlook.com>
2026-09-21 00:40:54 +0300

    Q06

---
+++ OPP04G03/DeliveryReport.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OPP04G03
+{
+    internal class DeliveryReport
+    {
+        public static void PrintShipment(ITrackable shipment)
+        {
+            Console.WriteLine(shipment.GetTrackingStatus());
+        }
+        public static void PrintInsurance(IInsurable shipment)
+        {
+            Console.WriteLine($"Insurance cost:{shipment.CalculateInsurance()}");
+        }
+    }
+}
