0e3ad51e5b44b92e30b4270ee0469172d2924974
passant ahmed <passantahmed992006@outlook.com>
2026-09-21 00:47:27 +0300

    Q08

--- OPP04G03/Program.cs
+++ OPP04G03/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace OPP04G03
+﻿using System.Net;
+
+namespace OPP04G03
 {
     internal class Program
     {
@@ -34,11 +36,45 @@
 
             //عندما نحتاج لتطبيق أكثر من سلوك(Multiple Inheritance) في نفس الكلاس.
 
-//            c)Multiple Abstract Classes: No(لا، C# تدعم الوراثة من كلاس واحد فقط).
+            //            c)Multiple Abstract Classes: No(لا، C# تدعم الوراثة من كلاس واحد فقط).
 
-//Multiple Interfaces: Yes(نعم، يمكن تطبيق عدة واجهات في نفس الوقت).
+            //Multiple Interfaces: Yes(نعم، يمكن تطبيق عدة واجهات في نفس الوقت).
             #endregion
 
+            #region 03
+
+            //StandardShipment standard = new StandardShipment("SH001", "Laptop", 5m, 70m);
+            //ExpressShipment express = new ExpressShipment("SH002", "Mobile Phone", 4m, 80m);
+            //International international = new International("SH003", "Documents", 12m, 200m);
+
+            //DeliveryCenter center = new DeliveryCenter();
+            //center.AddShipment(standard);
+            //center.AddShipment(express);
+            //center.AddShipment(international);
+
+            //Console.WriteLine("Delivery Center");
+            //Console.WriteLine();
+            //center.PrintAllShipments();
+
+            //Console.WriteLine("Tracking Status");
+            //Console.WriteLine();
+            //center.PrintTrackingstatus();
+
+            //Console.WriteLine();
+            //Console.WriteLine("Insurance");
+            //Console.WriteLine();
+
+            //ITrackable[] trackableShipments = new ITrackable[] { standard, express, international };
+
+            //IInsurable[] insurableShipments = new IInsurable[] { standard, express, international };
+
+            //DeliveryReport.PrintInsurance(standard);
+            //DeliveryReport.PrintInsurance(express);
+            //DeliveryReport.PrintInsurance(international);
+
+            //Console.WriteLine();
+            //Console.WriteLine("Interface Polymorphism Demonstrated Successfully.");
+            #endregion
         }
     }
 }
