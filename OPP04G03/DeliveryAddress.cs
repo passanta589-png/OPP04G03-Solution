e0c9432c539f923b126cbe2d4e18025639f5a672
passant ahmed <passantahmed992006@outlook.com>
2026-09-21 00:31:04 +0300

    Q03

---
+++ OPP04G03/DeliveryAddress.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OPP04G03
+{
+    internal class DeliveryAddress
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public DeliveryAddress(string street, string city, string state, string zipCode)
+        {
+            Street = street;
+            City = city;
+            State = state;
+            ZipCode = zipCode;
+        }
+
+        internal object GetFullAddress()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OPP04G03/Shipment.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OPP04G03
+{
+    internal abstract class Shipment
+    {
+        private string _Trackingcode;
+        private string _Description;
+        private decimal _Weight;
+        private decimal _DeliveryFee;
+
+        public string TrackingCode
+        {
+            get { return _Trackingcode; }
+            private set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    _Trackingcode = value;
+                }
+            }
+        }
+        public string Description
+
+        {
+            get { return _Description; }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    Description = value;
+                }
+            }
+
+        }
+        public decimal Weight
+        {
+            get => _Weight;
+            set
+            {
+                if (value > 0) _Weight = value;
+            }
+        }
+        public decimal DeliveryFee
+        {
+            get { return DeliveryFee; }
+
+            private set
+            {
+                if (value >= 0)
+                {
+                    DeliveryFee = value;
+                }
+            }
+
+        }
+        internal DeliveryAddress Destination { get; set; }
+
+        public abstract decimal estimatedCost { get; }
+
+        internal Shipment(string trackingCode, string description, decimal weight, decimal deliveryFee, DeliveryAddress destination)
+        {
+            TrackingCode = trackingCode;
+            Description = description;
+            Weight = weight;
+            DeliveryFee = deliveryFee;
+            Destination = destination;
+        }
+        public void updateWeight(decimal newWeight)
+        {
+            if (newWeight > 0)
+            {
+                Weight = newWeight;
+            }
+
+        }
+        public void updateWeightAndDeliveryFee(decimal newWeight, double extrapackingweight)
+        {
+            if (newWeight > 0 && extrapackingweight >= 0)
+            {
+                Weight = newWeight + (decimal)extrapackingweight;
+            }
+        }
+        public abstract void PrintShipment();
+    }
+}
